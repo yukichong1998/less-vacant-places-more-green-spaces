03684b196e53c02df3e452724e0a72a157bdeec6
Yuki Yu Qi Chong <yukichong@linux7.cs.uchicago.edu>
2022-03-03 21:39:22 -0600

    Merged hardship and health dfs into Dash Datatable

--- dash_datatable.py
+++ dash_datatable.py
@@ -1,21 +1,43 @@
 from dash import Dash, dash_table
 import pandas as pd
+from functools import reduce
+import data_cleaning as dc
 
-VAR_NAMES = {"GEOID":"Tract Code",
-            "HDX_2015-2019":"Hardship Score"}
+HARDSHIP_COLS = ["GEOID", "HDX_2015-2019"]
+VAR_NAMES = {
+    "GEOID":"census_tract",
+    "HDX_2015-2019":"Hardship Score",
+    }
+HARDSHIP_FILENAME = 'hardship_index.csv'
 
-def rename_cols(filename):
-    df = pd.read_csv(filename)
-    cols_to_keep = list(VAR_NAMES.keys())
-    df = df[cols_to_keep]
-    final_df = df.rename(columns=VAR_NAMES)
-    return final_df
+HEALTH_COLS = ["stcotr_fips", "est"]
 
-df = rename_cols('hardship_index.csv')
+# def rename_cols(filename):
+#     df = pd.read_csv(filename)
+#     cols_to_keep = list(VAR_NAMES.keys())
+#     df = df[cols_to_keep]
+#     final_df = df.rename(columns=VAR_NAMES)
+#     return final_df
+
+# Load hardship and health dataframes
+hardship = dc.load_data(HARDSHIP_FILENAME, HARDSHIP_COLS)
+physical_distress = dc.load_health_data("health_physical_distress.csv", HEALTH_COLS, "Physical Distress")
+mental_distress = dc.load_health_data("health_mental_distress.csv", HEALTH_COLS, "Mental Distress")
+diabetes = dc.load_health_data("health_diabetes.csv", HEALTH_COLS, "Diabetes")
+hbp = dc.load_health_data("health_high_blood_pressure.csv", HEALTH_COLS, "High Blood Pressure")
+life_expectancy = dc.load_health_data("health_life_expectancy.csv", HEALTH_COLS, "Life Expectancy")
+
+# Merge hardship and health dataframes
+dfs = [hardship, physical_distress, mental_distress, diabetes, hbp, life_expectancy]
+merged_df = reduce(lambda left,right: pd.merge(left,right,on='census_tract'), dfs)
 
 app = Dash(__name__)
 
-app.layout = dash_table.DataTable(df.to_dict('records'), [{"name": i, "id": i} for i in df.columns])
+app.layout = dash_table.DataTable(
+    data=merged_df.to_dict('records'), 
+    columns=[{"name": i, "id": i} for i in merged_df.columns],
+    style_cell={'fontSize':20, 'font-family':'sans-serif', 'padding': '5px'},
+    style_header={'fontWeight': 'bold'},)
 
 
 if __name__ == '__main__':
