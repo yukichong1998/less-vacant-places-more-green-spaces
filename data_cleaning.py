a91711b87da27791727f7237571d9d418a5a6331
Yuki Yu Qi Chong <yukichong@linux7.cs.uchicago.edu>
2022-03-08 17:40:09 -0600

    Minor edits

--- data_cleaning.py
+++ data_cleaning.py
@@ -4,30 +4,39 @@ This module handles the data cleaning process.
 
 import pandas as pd
 
+VAR_NAMES = {
+    "GEOID":"census_tract",
+    "GEOID10":"census_tract",
+    "COMMAREA":"community_area",
+    "AREA_NUMBER":"community_area",
+    "COMMUNITY":"Neighborhood",
+    "HDX_2015-2019":"Hardship Score",
+    }
+
+CENSUS_TRACT_COLS = ["GEOID10", "COMMAREA"]
+CENSUS_TRACT_FILENAME = 'census_tracts.csv'
+COMM_AREA_COLS = ["AREA_NUMBER", "COMMUNITY"]
+COMM_AREA_FILENAME = 'comm_areas.csv'
+
 PARKS_COLS = ["PARK", "LOCATION", "ZIP", "ACRES", "PARK_CLASS"]
 PARKS_FILENAME = 'CPD_Parks.csv'
 
 HARDSHIP_COLS = ["GEOID", "HDX_2015-2019"]
-VAR_NAMES = {
-    "GEOID":"census_tract",
-    "HDX_2015-2019":"Hardship Score",
-    }
 HARDSHIP_FILENAME = 'hardship_index.csv'
 
 HEALTH_COLS = ["stcotr_fips", "est"]
 
 
-def load_data(filename, col_list):
-    df = pd.read_csv(filename, header=0, usecols=col_list)
-    df = df.rename(columns=VAR_NAMES)
-    return df
-
-
-def load_health_data(filename, col_list, col_name=None):
+def load_data(filename, col_list, col_name=None):
     df = pd.read_csv(filename, header=0, usecols=col_list)
     if col_name:
         df = df.rename(columns = {"stcotr_fips":"census_tract", "est": col_name})
+    else:
+        df = df.rename(columns=VAR_NAMES)
     return df
+
+# Geographical data
+geo_data = load_data(CENSUS_TRACT_FILENAME, CENSUS_TRACT_COLS)
 
 # Parks data
 parks = load_data(PARKS_FILENAME, PARKS_COLS)
@@ -37,11 +46,11 @@ parks = parks.astype({"ZIP": str, "ACRES": float})
 hardship = load_data(HARDSHIP_FILENAME, HARDSHIP_COLS)
 
 # Health data
-physical_distress = load_health_data("health_physical_distress.csv", HEALTH_COLS, "Physical Distress")
-mental_distress = load_health_data("health_mental_distress.csv", HEALTH_COLS, "Mental Distress")
-diabetes = load_health_data("health_diabetes.csv", HEALTH_COLS, "Diabetes")
-hbp = load_health_data("health_high_blood_pressure.csv", HEALTH_COLS, "High Blood Pressure")
-life_expectancy = load_health_data("health_life_expectancy.csv", HEALTH_COLS, "Life Expectancy")
+physical_distress = load_data("health_physical_distress.csv", HEALTH_COLS, "Physical Distress")
+mental_distress = load_data("health_mental_distress.csv", HEALTH_COLS, "Mental Distress")
+diabetes = load_data("health_diabetes.csv", HEALTH_COLS, "Diabetes")
+hbp = load_data("health_high_blood_pressure.csv", HEALTH_COLS, "High Blood Pressure")
+life_expectancy = load_data("health_life_expectancy.csv", HEALTH_COLS, "Life Expectancy")
 
 health_data = physical_distress.merge(mental_distress).merge(diabetes).merge(hbp).merge(life_expectancy)
 health_data = health_data.astype({"census_tract": str})
@@ -49,7 +58,7 @@ health_data = health_data.astype({"census_tract": str})
 
 
 # Creating summary score of health indicators    
-def create_summary_score(data, metrics, tracts):
+def create_summary_score(data, metrics, neighborhood):
     '''
     Given the selected health metrics and tracts, creates a subset of
     data and a new column for summary score.
@@ -60,12 +69,13 @@ def create_summary_score(data, metrics, tracts):
         tracts: list of tracts (str) to filter for
         metrics: list of health indicators (str) to filter for
     '''
-    filter_tract = data["census_tract"].isin(tracts)
-    cols_to_keep = ["census_tract"] + metrics
-    subset = data.loc[filter_tract, cols_to_keep]
+    filter_neighborhood = data["Neighborhood"].isin(neighborhood)
+    cols_to_keep = ["Neighborhood"] + metrics
+    subset = data.loc[filter_neighborhood, cols_to_keep]
     print("subset:", subset)
 
     subset["summary_score"] = sum([subset[i] for i in metrics])
+    print("Health Score:", subset["summary_score"])
     return subset
 
 
