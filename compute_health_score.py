7417e57043471b42da8ea3a472d4516377da08b8
Yuki Yu Qi Chong <yukichong@linux7.cs.uchicago.edu>
2022-03-10 11:53:56 -0600

    Updated functions

--- compute_health_score.py
+++ compute_health_score.py
@@ -6,29 +6,21 @@ import data_cleaning as dc
 
 
 HEALTH_COLS = ["stcotr_fips", "est"]
-physical_distress = dc.load_data("health_physical_distress.csv", HEALTH_COLS, "Physical Distress")
-mental_distress = dc.load_data("health_mental_distress.csv", HEALTH_COLS, "Mental Distress")
-diabetes = dc.load_data("health_diabetes.csv", HEALTH_COLS, "Diabetes")
-hbp = dc.load_data("health_high_blood_pressure.csv", HEALTH_COLS, "High Blood Pressure")
 life_expectancy = dc.load_data("health_life_expectancy.csv", HEALTH_COLS, "Life Expectancy")
-
-
-merged_dfs = dc.merge_all_dfs()
-df = dc.tract_to_neighborhood(merged_dfs)
-
 AVG_LIFE_EXP = life_expectancy["Life Expectancy"].mean()
 HEALTH_INDICATORS = ["Physical Distress", "Mental Distress", "Diabetes", 
                     "High Blood Pressure", "Life Expectancy"]
 OTHER_INDICATORS = ["Hardship Score"]
-                    
+
+merged_dfs = dc.merge_all_dfs()
+df = dc.tract_to_neighborhood(merged_dfs)
+                
 def compute_health_score(df, metrics):
     if "Life Expectancy" in metrics:
-        # subset["relative_life_exp"] = AVG_LIFE_EXP - df["Life Expectancy"]
         # Weight metrics by whether life expectancy is above or below the mean
         early_death = df["Life Expectancy"] < AVG_LIFE_EXP
         df.loc[early_death, metrics] = df.loc[early_death, metrics] * 1.1
         metrics.remove("Life Expectancy")
-
     weight = 100 / len(metrics)
     subset = df[metrics] / 100 * weight
     subset["Health Risk Score"] = subset[metrics].sum(axis=1).round(1)
