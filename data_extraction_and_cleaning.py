82ac7bb63dc480d8a94769ed5b139d9954f8c733
Dustin Marshall <56142954+dustinmarshall@users.noreply.github.com>
2022-03-16 10:28:12 -0500

    Update and rename community_areas.py to data_extraction_and_cleaning.py

--- data_extraction_and_cleaning.py
+++ data_extraction_and_cleaning.py
@@ -5,37 +5,37 @@ from shapely.geometry import Point, Polygon
 from pyproj import Geod
 from shapely import wkt
 
-def gen_community_areas():
-    """
-    Pulls data from City of Chicago and Cook County Data portals, then cleans and transforms the data to be used for
-    analysis, then generates the community_areas.csv, parks.csv, and vacants.csv files.
+"""
+The following code pulls data from City of Chicago and Cook County Data portals, then 
+cleans and transforms the data to be used for analysis, then generates the community_areas.csv, 
+parks.csv, and vacants.csv files.
     
-    Inputs:
-        None
+Inputs:
+    None
     
-    Outputs:
-        community_areas: csv file containing the following columns 
-            Neighborhood: (str) community area
-            vacant_count: (int) total count of city-owned vacancies by community area
-            vacant_acres: (float) total amount of city-owned vacancy acreage by community area
-            park_count: (int) total count of parks by community area
-            park_acres: (float) total amount of park acreage by community area
-            census_tracts: (list) census tract polygons by community area
-            community_area_polygons: (list) polygons within community area
-            park_polygons: (list) park polygons within community area
+Outputs:
+    community_areas: csv file containing the following columns 
+        Neighborhood: (str) community area
+        vacant_count: (int) total count of city-owned vacancies by community area
+        vacant_acres: (float) total amount of city-owned vacancy acreage by community area
+        park_count: (int) total count of parks by community area
+        park_acres: (float) total amount of park acreage by community area
+        census_tracts: (list) census tract polygons by community area
+        community_area_polygons: (list) polygons within community area
+        park_polygons: (list) park polygons within community area
         parks: csv file containing the following columns
-            index: (str) park name
-            latitude: (float)
-            longitude: (float)
-            size: (float) normalized park acreage
-            community_area_name: (str)
-        vacants: csv file containing the following columns
-            index: (str) uniquely identifying pin
-            latitude: (int)
-            longitude: (int)
-            community_area_name: (str)
-            size: (float) normalized lot acres
-    """
+        index: (str) park name
+        latitude: (float)
+        longitude: (float)
+        size: (float) normalized park acreage
+        community_area_name: (str)
+    vacants: csv file containing the following columns
+        index: (str) uniquely identifying pin
+        latitude: (int)
+        longitude: (int)
+        community_area_name: (str)
+        size: (float) normalized lot acres
+"""
 
     # Access Cook County & Chicago Data Portal API
     cook_county = Socrata("datacatalog.cookcountyil.gov", None)
@@ -161,6 +161,3 @@ def gen_community_areas():
     community_areas_df.reset_index(inplace=True)
     community_areas_df = community_areas_df.rename(columns = {'index':'Neighborhood'})
     community_areas_df.to_csv('community_areas.csv', index=True)
-    
-    # Return main dataframe (for optional use)
-    return community_areas_df
